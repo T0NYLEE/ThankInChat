dfc5018e81967dbba998a83c8d840238f73e3018
Tony Lee <852590@qq.com>
2023-05-12 14:31:44 +0800

    添加微信扫码登陆component

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import { NConfigProvider } from 'naive-ui'
 import { NaiveProvider } from '@/components/common'
 import { useTheme } from '@/hooks/useTheme'
 import { useLanguage } from '@/hooks/useLanguage'
+import wxlogin from './components/common/WXLogin/wxlogin.vue'
 
 const { theme, themeOverrides } = useTheme()
 const { language } = useLanguage()
+const appid = 'wx28fe4099f54d1edf'
+const scope = 'snsapi_login'
+const redirect_uri = 'http://127.0.0.1:1002'
 </script>
 
 <template>
@@ -15,6 +19,7 @@ const { language } = useLanguage()
     :theme-overrides="themeOverrides"
     :locale="language"
   >
+	<wxlogin :appid="appid" :scope="scope" :redirect_uri="redirect_uri"></wxlogin>
     <NaiveProvider>
       <RouterView />
     </NaiveProvider>
