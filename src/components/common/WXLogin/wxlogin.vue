8453f9ef9769092a66a91af3db119cd734e5c2f8
wxl <wxl@thankin.com>
2023-05-15 14:25:32 +0800

    循环获取消息后，隐藏二维码

--- src/components/common/WXLogin/wxlogin.vue
+++ src/components/common/WXLogin/wxlogin.vue
@@ -8,37 +8,34 @@ import {ref,onMounted} from 'vue'
 import {NModal} from 'naive-ui'
 import {getQrCode,getLoginState} from '@/api'
 	const setSrc: any =ref();
-	let show=true;
-	onMounted(() => {
+	const show=ref(true);
+	let intervalId: any=null;
+	onMounted(()=>{
 		createQrocde();
 	})
-	const createQrocde = async () => {
-		try {
+	const createQrocde=async()=>{
+		try{
 			const uuid=guid2();
-			const ticket: any = await getQrCode(uuid);
+			const ticket:any=await getQrCode(uuid);
 			setSrc.value=`https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=${ticket.ticket}`;
-			getState(uuid);
-		}
-		catch (error: any) {
-			console.log(error)
-		}
+			getState('75bf0717-c727-4476-9746');
+		}catch(e){console.error(e)}
 	}
 	function guid2(){
-		let code = '';
-		for(var i=0;i<10;i++){
-    		code += parseInt((Math.random()*10).toString());
-		}
-		return code;
+		return 'xxxxxxxx-xxxx-4xxx-yxxx'.replace(/[xy]/g,function(c){
+			const r=Math.random()*16|0,v=c==='x'?r:(r&0x3|0x8);
+			return v.toString(16);
+		});
 	}
 	function getState(uuid:string){
-		setInterval(async ()=>{
-			const msg: any = await getLoginState(uuid);
+		intervalId=setInterval(async ()=>{
+			const msg:any=await getLoginState(uuid);
 			if(msg.msg=='Success'){
 				console.log(msg.data)
-				show=false;
-				clearInterval();
+				show.value=false;
+				clearInterval(intervalId);
 			}
-		}, 1000);
+		}, 3000);
 		
 	}
 </script>
