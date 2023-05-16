67bc1c95ef00923619a9f4e5489499de3056f6cc
wxl <wxl@thankin.com>
2023-05-16 09:30:56 +0800

    Revert "显示human的结果"
    This reverts commit 9cf34384ff18babbcfd42b50fa336007a83c9fd4.

--- src/views/admin/humans.vue
+++ src/views/admin/humans.vue
@@ -34,7 +34,7 @@ onMounted(()=>{
 })
 async function getHumans(){
 	try {
-		data.value=(await fetchHumanALL()).message
+		data.value=(await fetchHumanALL()).data
 	}catch(e){console.error(e)}
 }
 const message=useMessage()
