472cc2626e695b12bad673f859767b4ac7fbb183
Tony Lee <852590@qq.com>
2023-05-17 00:20:50 +0800

    在ts中测试格式化插件

--- src/components/common/WXLogin/wxlogin.vue
+++ src/components/common/WXLogin/wxlogin.vue
@@ -1,19 +1,19 @@
 <template>
-	<NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 640px">
+	<NModal v-model:show="show" :auto-focus="false" preset="card" style="width:95%; max-width:640px">
 		<iframe sandbox="allow-scripts allow-top-navigation allow-same-origin" scrolling="no" width="400" height="400" frameBorder="0" allowTransparency="true" :src="setSrc"></iframe>
 	</NModal>
 </template>
 <script setup lang='ts'>
-import {ref,onMounted} from 'vue'
-import {NModal} from 'naive-ui'
-import {getQrCode,getLoginState,fetchPost} from '@/api'
-import {ss} from '@/utils/storage'
-	const LOCAL_NAME = 'userStorage'
+import{ref,onMounted}from 'vue'
+import{NModal}from 'naive-ui'
+import{getQrCode,getLoginState,fetchPost}from '@/api'
+import{ss}from '@/utils/storage'
+	const LOCAL_NAME='userStorage'
 	const setSrc:any=ref();
 	const show=ref(false);
 	let intervalId:any=null;
 	onMounted(()=>{
-		const localSetting:any= ss.get(LOCAL_NAME)
+		const localSetting:any=ss.get(LOCAL_NAME)
 		if(localSetting==undefined||localSetting==null){
 			show.value=true;
 			createQrocde();
@@ -37,17 +37,17 @@ import {ss} from '@/utils/storage'
 		intervalId=setInterval(async ()=>{
 			const msg:any=await getLoginState(uuid);
 			if(msg.msg=='Success'){
-				ss.set(LOCAL_NAME, {userInfo: {avatar: msg.data.avatar,name: msg.data.nickname,openid:msg.data.openid},})
+				ss.set(LOCAL_NAME,{userInfo:{avatar:msg.data.avatar,name:msg.data.nickname,openid:msg.data.openid},})
 				const token=await fetchPost('HumanAdd',msg.data);
-				ss.set('token', {token: {'token':token}})
+				ss.set('token',{token:{'token':token}})
 				console.log(token)
 				await getQrCode(uuid);
 				show.value=false;
 				clearInterval(intervalId);
 				window.location.reload()
 			}
-		}, 3000);
-		
+		},3000);
+
 	}
 </script>
 <style scoped>
