465e43049e2b1732128187fd0d12ec21a045094a
Tony Lee <852590@qq.com>
2023-05-18 00:06:49 +0800

    所有post添加token，实现后端验证token逻辑

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -46,6 +46,13 @@ function http<T = any>(
 
   const params = Object.assign(typeof data === 'function' ? data() : data ?? {}, {})
 
+	//header添加token
+	const user=localStorage.getItem('user')
+	if(user){
+		const token=JSON.parse(user).token
+		headers = Object.assign(headers ?? {}, { Authorization: `Bearer ${token}` })
+	}
+
   return method === 'GET'
     ? request.get(url, { params, signal, onDownloadProgress }).then(successHandler, failHandler)
     : request.post(url, params, { headers, signal, onDownloadProgress }).then(successHandler, failHandler)
